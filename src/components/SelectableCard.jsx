4bd46d8299ae6653021eee6831e66f9a213d1894
Tina Thomas <tinamariathomas@gmail.com>
2025-07-23 09:28:18 -0500

    Set up single card with image and description

---
+++ src/components/SelectableCard.jsx
@@ -0,0 +1,32 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+
+function SelectableCard(props) {
+    const { title, description, image } = props;
+    return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    );
+  }
+
+  export default SelectableCard;
